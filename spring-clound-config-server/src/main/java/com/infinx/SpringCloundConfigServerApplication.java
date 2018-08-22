1f087be03f79319a7ec768df972d777a89a61eb3
santoshturerao <tureraosantosh@gmail.com>
2018-08-23 00:01:05 +0530

    intial

---
+++ spring-clound-config-server/src/main/java/com/infinx/SpringCloundConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.infinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloundConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloundConfigServerApplication.class, args);
+	}
+}
